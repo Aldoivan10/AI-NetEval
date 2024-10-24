2ed0510a5531ffbf0fe9ae8fd51435a6253cc827
Aldoivan <aldoivan_palma@hotmail.com>
2024-10-23 21:38:02 -0600

    Fixed: Separated utils for tests Separate utilities for testing and operation

---
+++ util/aitest.py
@@ -0,0 +1,45 @@
+from cv2.typing import MatLike, Rect, Point, Vec3i
+from PIL.ImageDraw import Draw as drawer
+from typing import List
+from PIL import Image
+
+# Función para generar una sequencia
+def infinite_sequence():
+    num = 1
+    while True:
+        yield num
+        num += 1
+
+# Función para dibujar polígonos
+def draw_poly(img: Image.Image, points: tuple[Point, ...], color: Vec3i = (255, 0, 0), width: int = 1):
+    draw = drawer(img)
+    draw.polygon(points, None, color, width)
+
+# Función para dibujar texto
+def draw_text(img: Image.Image, text: str, container: Rect, font_size: int = 12, fill: Vec3i = (255, 0, 0),  width: int = 1) -> None:
+    draw = drawer(img)
+    # Obtenemos [x, y, ancho, alto] del contenedor
+    [cx, cy, cw, ch] = container 
+    # Obtenemos el ancho y alto del texto
+    [_, _, tw, th] = draw.textbbox((0,0), text, stroke_width=width, font_size=font_size) 
+    # A las dimenciones obtenemos la mitad
+    tw, th = tw // 2, th // 2                
+    # Obtenemos el centro del padre                                             
+    [px, py] = ((cx + cw / 2), (cy + ch / 2))
+    # Calculamos el nuevo origen centrado
+    orig = (px - tw, py - th)
+    # Dibujamos un outline
+    draw.text(orig, text, (255,255,255), stroke_width=width * 3, font_size=font_size)
+    # Dibujamos el texto
+    draw.text(orig, text, fill, stroke_width=width, font_size=font_size)
+
+# Función para dibujar contornos
+def draw_cnts(img: Image.Image, cnts: List[MatLike]):
+    # Iniciamos la secuencia (Solo para indicar el orden)
+    seq = infinite_sequence()
+    # Por cada contorno
+    for poly, rect in cnts:
+        # Dibujamos el polígono
+        draw_poly(img, [tuple(row) for row in poly], width=5)
+        # Dibujamos las dimensiones
+        draw_text(img,  f"{next(seq)}: {rect[2]} x {rect[3]}", rect, fill=(0,0,255), width=2, font_size=30)
--- util/aiutil.py
+++ util/aiutil.py
@@ -1,6 +1,5 @@
 from cv2.typing import MatLike, Rect, Point, Vec3i
 import imutils.perspective as perspective
-from PIL.ImageDraw import Draw as drawer
 import imutils.contours as cnts
 from typing import List
 from PIL import Image
@@ -8,13 +7,6 @@ import numpy as np
 
 import cv2
 import imutils
-
-# Función para generar una sequencia
-def infinite_sequence():
-    num = 1
-    while True:
-        yield num
-        num += 1
 
 # Función para aproximar el contorno a un polígono, regular o irregular
 def approx_contour(contour: MatLike, regular: bool = True) -> tuple[MatLike, Rect]:
@@ -52,40 +44,6 @@ def get_countours(img: Image.Image,  min_h: int = 100) -> List[MatLike]:
     contours = [(poly, rect) for poly, rect in contours if rect[-1] >= min_h and len(poly) == 4]
     # Retornamos los contornos
     return contours
-
-# Función para dibujar polígonos (Solo para pruebas)
-def draw_poly(img: Image.Image, points: tuple[Point, ...], color: Vec3i = (255, 0, 0), width: int = 1):
-    draw = drawer(img)
-    draw.polygon(points, None, color, width)
-
-# Función para dibujar texto (Solo para pruebas)
-def draw_text(img: Image.Image, text: str, container: Rect, font_size: int = 12, fill: Vec3i = (255, 0, 0),  width: int = 1) -> None:
-    draw = drawer(img)
-    # Obtenemos [x, y, ancho, alto] del contenedor
-    [cx, cy, cw, ch] = container 
-    # Obtenemos el ancho y alto del texto
-    [_, _, tw, th] = draw.textbbox((0,0), text, stroke_width=width, font_size=font_size) 
-    # A las dimenciones obtenemos la mitad
-    tw, th = tw // 2, th // 2                
-    # Obtenemos el centro del padre                                             
-    [px, py] = ((cx + cw / 2), (cy + ch / 2))
-    # Calculamos el nuevo origen centrado
-    orig = (px - tw, py - th)
-    # Dibujamos un outline
-    draw.text(orig, text, (255,255,255), stroke_width=width * 3, font_size=font_size)
-    # Dibujamos el texto
-    draw.text(orig, text, fill, stroke_width=width, font_size=font_size)
-
-# Función para dibujar contornos (Solo para pruebas)
-def draw_cnts(img: Image.Image, cnts: List[MatLike]):
-    # Iniciamos la secuencia (Solo para indicar el orden)
-    seq = infinite_sequence()
-    # Por cada contorno
-    for poly, rect in cnts:
-        # Dibujamos el polígono
-        draw_poly(img, [tuple(row) for row in poly], width=5)
-        # Dibujamos las dimensiones
-        draw_text(img,  f"{next(seq)}: {rect[2]} x {rect[3]}", rect, fill=(0,0,255), width=2, font_size=30)
 
 # Función para obtener las respuestas de una columna
 def get_responses(img: Image.Image, countours: List[MatLike], rows: int = 10) -> List[List[Image.Image]]:
