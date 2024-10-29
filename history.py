b1e0167eebc710ec3d722dc60e6a31b64492fa9b
Aldoivan <aldoivan_palma@hotmail.com>
2024-10-28 20:19:37 -0600

    Added: Script for display graphics and structure of models

---
+++ history.py
@@ -0,0 +1,47 @@
+import keras.api.models as models
+import matplotlib.pyplot as plt
+import json
+import sys
+
+plt.style.use("./pacoty.mplstyle")
+
+args = sys.argv
+model = 'mish_augmented' if len(args) == 1 else args[1]
+path = f"./models/{model}"
+
+#  Cargar modelo
+model = models.load_model(f"{path}/best_model.keras")
+# Mostrar el modelo
+model.summary()
+
+# Abrir el historial
+with open(f"{path}/history.json", 'r') as file:
+    history_loaded = json.load(file)
+
+# Extraer la pérdida y precisión
+loss = history_loaded['loss']
+val_loss = history_loaded['val_loss']
+accuracy = history_loaded['accuracy']
+val_accuracy = history_loaded['val_accuracy']
+
+# Graficar la pérdida
+plt.figure(figsize=(12, 5))
+plt.subplot(1, 2, 1)
+plt.plot(loss, label='Pérdida de Entrenamiento')
+plt.plot(val_loss, label='Pérdida de Validación')
+plt.title('Pérdida')
+plt.xlabel('Épocas')
+plt.ylabel('Pérdida')
+plt.legend()
+
+# Graficar la precisión
+plt.subplot(1, 2, 2)
+plt.plot(accuracy, label='Precisión de Entrenamiento')
+plt.plot(val_accuracy, label='Precisión de Validación')
+plt.title('Precisión')
+plt.xlabel('Épocas')
+plt.ylabel('Precisión')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
