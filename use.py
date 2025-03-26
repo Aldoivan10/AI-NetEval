bf83d4d74ab380a4e37aac06f93395e4049ff06c
Aldoivan10 <apalmac002@alumno.uaemex.mx>
2025-03-25 19:07:26 -0600

    Refactor: use code

--- use.py
+++ use.py
@@ -1,51 +1,14 @@
-import keras.api.preprocessing as preprocessing
-import keras.api.models as  models
-from util import aitest
-import numpy as np
+""" import keras.api.models as  models """
+from PIL import Image
 
 # Direcciones de los archivos
 model_path = "./model/mish/best_model.keras"
-test_path = "./images/Test"
-img_path = f"{test_path}/A/A_1.jpg"
+exam_path = "./images/Prueba.jpg"
 
-# Cargamos el modelo ya entrenado
-model = models.load_model(model_path)
+""" # Cargamos el modelo ya entrenado
+model = models.load_model(model_path) """
 
-################################################################################
-### Predecir un DATASET                                                      ###
-################################################################################
+# Cargamos el examen y la convertimos a escala de grises
+def_size = (1128, 1226)          # Tamaño por defecto
+img = Image.open(exam_path).convert("L")
 
-# Cargamos el dataset de prueba (No olvidar que debe estar en escala de grises)
-test_ds = preprocessing.image_dataset_from_directory(test_path, color_mode="grayscale")
-# Obtenemos los nombres de las clases
-class_names = np.array(test_ds.class_names)
-# Obtenemos las respuestas, por cada conjunto en el dataset
-for batch, _ in test_ds:
-    # Procesar el batch completo
-    results = model(batch)
-    # Obtener las predicciones para todo el batch
-    predictions = class_names[np.argmax(results, axis=1)]  
-    # Mostramos los resultados por batch
-    aitest.show_responses(batch, predictions)
-
-################################################################################
-### Predecir una IMAGEN                                                      ###
-################################################################################
-
-# Cargamos una imagen, estas imágenes deben tener el mismo tamaño
-# que las de entrenamiento y estar en escala de grises
-img = preprocessing.image.load_img(img_path, color_mode="grayscale")
-# Mostramos la imagen (Solo para pruebas)
-img.show()
-# Conertimos la imagen en un arreglo numpy
-img = np.array(img)
-# Incrementammos una dimension (esta dimensión indica el número de batch)
-img = np.expand_dims(img, axis=0)
-# Hacemos la predicción
-prediction = model(img)
-# Obtenemos el número de clase que es más probable
-class_index = np.argmax(prediction)
-# Obtenemos el nombre de la clase
-class_name = class_names[class_index]
-# Mostramos el resultado (Solo para pruebas)
-print(f"La clase más probable es: {class_name}")
